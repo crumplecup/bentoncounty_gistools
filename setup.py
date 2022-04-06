0228106d17c81ff175ffe41e7968b86a0c8ec37a
crumplecup <erik.w.rose@gmail.com>
2022-04-06 05:21:07 -0700

    test commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="bentoncounty_gistools",
+    version="0.0.1",
+    author="Erik Rose",
+    author_email="erik.w.rose@gmail.com",
+    description="Mapping tools for Benton County Community Development, Oregon",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crumplecup/bentoncounty_gistools/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
