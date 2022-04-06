fe2c5b44951366c55f6010e8efd035f49788de35
crumplecup <erik.w.rose@gmail.com>
2022-04-06 15:55:38 -0700

    documentation test

--- bentoncounty_gistools/bentoncounty_gistools.py
+++ bentoncounty_gistools/bentoncounty_gistools.py
@@ -2,31 +2,37 @@ import random
 import string
 from arcgis.mapping import MapServiceLayer
 
-"""
-Takes a service and returns a list of layer urls.
-"""
 def layer_urls(item):
+    """List service layer urls.
+
+    Keyword arguments:
+    item -- ArcGIS Feature Collection Service
+    """
     urls = []
     for lyr in item.layers:
         urls.append(lyr.url)
     return urls
 
-"""
-Copied verbatim from https://community.esri.com/t5/arcgis-api-for-python-questions/python-api-add-group-layer-to-webmap/td-p/1112126
-
-To build a web map from a published service, we generate feature layers pointed to each service. Each feature layer requires a unique layer id, produced by this function.
-"""
-# generates random ids for layers
 def create_layer_id(layerIndex):
+    """Generate random ids for layers.
+    Copied verbatim from https://community.esri.com/t5/arcgis-api-for-python-questions/python-api-add-group-layer-to-webmap/td-p/1112126. To build a web map from a published service, we generate feature layers pointed to each service. Each feature layer requires a unique layer id, produced by this function.
+
+    Keyword arguments:
+    layerIndex -- Layer index number.
+    """
     return ''.join(random.choices(string.ascii_lowercase + \
             string.digits, k=11)) + "-layer-" + str(layerIndex)
 
 
-"""
-To create group layers, we wrap each service in a feature layer. This function formats the feature layer based upon the service layer.
-"""
 # generate feature class data for layer
 def fc_gen(layer, opacity=1):
+    """
+    To create group layers, we wrap each service in a feature layer. This function formats the feature layer based upon the service layer.
+
+    Keyword arguments:
+    layer -- MapServiceLayer source for feature layer.
+    opacity -- Opacity of feature layer.
+    """
     fc_dict = {}
     fc_dict.update({"id": create_layer_id(random.randint(10000, 99999))})
     fc_dict.update({"url": layer.url})
