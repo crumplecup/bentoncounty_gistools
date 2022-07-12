fe11d6a0a9028e9a1c1cd1d8696393885e57dd5c
crumplecup <erik.w.rose@gmail.com>
2022-07-11 20:17:16 -0700

    dependencies for setup

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + "/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = ["<arcgis> >= <2.0.1>"]
 requirements_dev = []
 
 setup(
     name="bentoncounty_gistools",
-    version="0.0.1",
+    version="0.0.3",
     author="Erik Rose",
     author_email="erik.w.rose@gmail.com",
     description="Mapping tools for Benton County Community Development, Oregon",
@@ -16,7 +24,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/crumplecup/bentoncounty_gistools/",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=install_requires,
     tests_requires=requirements_dev,
     classifiers=[
         "Programming Language :: Python :: 3.10",
